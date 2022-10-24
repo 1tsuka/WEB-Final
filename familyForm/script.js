d88fe57b5df38f03e73119ed7ee8923b4eb51d63
1tsuka <85206885+1tsuka@users.noreply.github.com>
2022-10-24 19:14:29 +0800

    Delete familyForm directory

--- familyForm/script.js
+++
@@ -1,8 +0,0 @@
-function needVideo(){
-    if(document.getElementById('yes').checked){
-        document.getElementById('video').style.display = 'inline-block';
-    }
-    else if(document.getElementById('no').checked){
-        document.getElementById('video').style.display = 'none';
-    }
-}
