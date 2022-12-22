3923774f1cfecebad4ec2c97f8cb774c76c42488
1tsuka <85206885+1tsuka@users.noreply.github.com>
2022-12-22 10:58:34 +0800

    Delete pages directory

--- pages/familyInfo/script.js
+++
@@ -1,9 +0,0 @@
-function toModify(){
-    window.location.href="../familyForm/familyForm.html"
-}
-
-var headerUrl = '../../template/header/header.tql'
-
-$.get(headerUrl,function(result){
-    $('#header').html(result);
-})
